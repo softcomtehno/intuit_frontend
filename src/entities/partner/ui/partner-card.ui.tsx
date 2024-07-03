a502b744001c4ce40deb5a3f98a31c358aaf541d
ma1abakiev <malabakievramzan@gmail.com>
2024-07-03 17:29:25 +0600

    style partner-card

--- src/entities/partner/ui/partner-card.ui.tsx
+++ src/entities/partner/ui/partner-card.ui.tsx
@@ -3,7 +3,7 @@ import { Partner } from '../partner.types'
 
 export const PartnerCard = ({ logo, title }: Partner) => {
   return (
-    <Card className="bg-[#ECEDF0] w-64 h-24  mx-5 flex  items-center justify-center gap-3 rounded-xl shadow-none">
+    <Card className="bg-[#ECEDF0] w-64 h-24  mx-2 flex  items-center justify-center gap-3 rounded-xl shadow-none">
       <img src={logo} alt={title} className="h-12 w-12 rounded-full " />
       <Typography variant="body1" className="font-bold uppercase">
         {title}
