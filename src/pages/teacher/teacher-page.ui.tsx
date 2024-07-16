68042d8ac44c1372f26da3affe4882e9b359b717
ma1abakiev <malabakievramzan@gmail.com>
2024-07-16 17:44:10 +0600

    add adaptive for teachers pages

--- src/pages/teacher/teacher-page.ui.tsx
+++ src/pages/teacher/teacher-page.ui.tsx
@@ -40,7 +40,7 @@ export const TeacherPage = () => {
   if (isSuccess) {
     return (
       <section
-        className="bg-cover bg-center h-screen flex items-center justify-center"
+        className="bg-cover bg-center  flex items-center justify-center py-20"
         style={{
           backgroundImage:
             'url("https://img2.akspic.ru/crops/8/2/9/0/4/140928/140928-nebo-gora-gornyj_hrebet-minimalizm-televideniye-3840x2160.jpg")',
@@ -56,7 +56,7 @@ export const TeacherPage = () => {
                   className="w-36 h-36 border-3 border-solid border-black rounded-full shadow-lg"
                 />
               </div>
-              <Typography variant="h4" className="font-bold text-white">
+              <Typography variant="h4" className="font-bold text-white md:text-center">
                 {staffData.data.name}
               </Typography>
               <Typography
@@ -67,47 +67,59 @@ export const TeacherPage = () => {
               </Typography>
             </div>
             <div className="flex flex-wrap justify-center gap-5 ">
-              <a
-                href={staffData.data.whatsapp}
-                target="_blank"
-                className="flex items-center justify-around text-white m-1 py-3 px-5 w-1/6 bg-green rounded-full transition-all hover:scale-105"
-              >
-                <WhatsApp className="mr-2" />
-                <span className="text-lg">WhatsApp</span>
-              </a>
-              <a
-                href={staffData.data.telegram}
-                target="_blank"
-                className="flex items-center justify-around text-white m-1 py-3 px-5 w-1/6 bg-[#32A9E1] rounded-full transition-all hover:scale-105"
-              >
-                <Telegram className="mr-2" />
-                <span className="text-lg">Telegram</span>
-              </a>
-              <a
-                href={staffData.data.instagram}
-                target="_blank"
-                className="flex items-center justify-around transition-all text-white m-1 py-3 px-5 w-1/6  rounded-full hover:scale-105 "
+              <div className="bg-green rounded-full transition-all hover:scale-105">
+                <a
+                  href={staffData.data.whatsapp}
+                  target="_blank"
+                  className="flex items-center justify-around text-white m-1 py-3 px-5 w-1/6 "
+                >
+                  <WhatsApp className="mr-2" />
+                  <span className="text-lg">WhatsApp</span>
+                </a>
+              </div>
+              <div className="bg-[#32A9E1] rounded-full transition-all hover:scale-105">
+                <a
+                  href={staffData.data.telegram}
+                  target="_blank"
+                  className="flex items-center justify-around text-white m-1 py-3 px-5 w-1/6 "
+                >
+                  <Telegram className="mr-2" />
+                  <span className="text-lg">Telegram</span>
+                </a>
+              </div>
+              <div
                 style={gradientStyle}
+                className=" rounded-full transition-all hover:scale-105"
               >
-                <Instagram className="mr-2" />
-                <span className="text-lg">Instagram</span>
-              </a>
-              <a
-                href={staffData.data.facebook}
-                target="_blank"
-                className="flex items-center justify-around text-white m-1 py-3 px-5 w-1/6 bg-[#4867AA]  rounded-full transition duration-300 hover:scale-105"
-              >
-                <Facebook className="mr-2" />
-                <span className="text-lg">Facebook</span>
-              </a>
-              <a
-                href={staffData.data.cv}
-                target="_blank"
-                className="flex items-center justify-around text-white m-1 py-3 px-5 w-1/6 bg-[#383838] rounded-full transition duration-300 hover:scale-105"
-              >
-                <ContactPage className="mr-2" />
-                <span className="text-lg">Резюме</span>
-              </a>
+                <a
+                  href={staffData.data.instagram}
+                  target="_blank"
+                  className="flex items-center justify-around transition-all text-white m-1 py-3 px-5 w-1/6  "
+                >
+                  <Instagram className="mr-2" />
+                  <span className="text-lg">Instagram</span>
+                </a>
+              </div>
+              <div className="bg-[#4867AA] rounded-full transition-all hover:scale-105">
+                <a
+                  href={staffData.data.facebook}
+                  target="_blank"
+                  className="flex items-center justify-around text-white m-1 py-3 px-5 w-1/6 "
+                >
+                  <Facebook className="mr-2" />
+                  <span className="text-lg">Facebook</span>
+                </a>
+              </div>
+              <div className="bg-[#383838] rounded-full transition-all hover:scale-105">
+                <a
+                  href={staffData.data.cv}
+                  target="_blank"
+                  className="flex items-center justify-around text-white m-1 py-3 px-5 w-1/6 "
+                >
+                  <ContactPage className="mr-2" />
+                  <span className="text-lg">Резюме</span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
