b17ed22503d9cff4fd4b5e557c0a9417c423051d
ma1abakiev <example@gmail.com>
2024-10-23 15:52:35 +0600

    save changes

--- src/entities/programs/programs.api.ts
+++ src/entities/programs/programs.api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://intuit.makalabox.com/api/';
+const API_URL = 'https://intuit.makalabox.com/api/university/';
 
 export function getProgramsQuery() {
   return axios.get(`${API_URL}programs/`);
