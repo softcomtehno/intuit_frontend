8f5d9d80d9e3c58a1653015fb7e915c005329f95
ma1abakiev <malabakievramzan@gmail.com>
2024-12-02 15:38:20 +0600

    design: education-form

--- src/widgets/education-form/ui/education-form-card.ui.tsx
+++ src/widgets/education-form/ui/education-form-card.ui.tsx
@@ -1,13 +1,17 @@
-export const EducationFormCard = ({ title, text}) => {
+import WbSunnyIcon from '@mui/icons-material/WbSunny'
+import { Icon } from '@mui/material'
+
+export const EducationFormCard = ({ title, text }) => {
   return (
-    <div className="p-5 w-[350px] h-[320px] bg-gray rounded-lg flex flex-col justify-between">
+    <div className="p-5 w-full h-[170px] bg-gray rounded-lg flex flex-col justify-between">
       <div className="flex justify-between items-center">
         <h4 className="text-3xl font-bold">{title}</h4>
-        <img src="icons8-солнце-50.png" alt="" />
+        <div className="rounded-full bg-green p-2 ">
+        {/* <WbSunnyIcon ></WbSunnyIcon> */}
+        <Icon component={WbSunnyIcon} className=""></Icon>
+        </div>
       </div>
-      <p>
-        {text}
-      </p>
+      <p>{text}</p>
     </div>
   )
 }
