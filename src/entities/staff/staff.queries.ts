85c66c75e0e4a260b1947a1f84fb880756fbecbf
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2025-01-07 11:47:10 +0600

    feat: merge conflict resolve

--- src/entities/staff/staff.queries.ts
+++ src/entities/staff/staff.queries.ts
@@ -1,23 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
-<<<<<<< HEAD
-import { getSortStaffs, getStaffDetailsQuery, getStaffLevels, getStaffQuery } from './staff.api'
-=======
 import {
   getSortStaffs,
   getStaffDetailsQuery,
   getStaffQuery,
   getStaffPositions,
 } from './staff.api'
->>>>>>> 2a74b0f53c03da37660cff222ab77700eeb6bf23
 
 const keys = {
   root: () => ['staff'],
   staff: (slug: string) => [...keys.root(), 'bySlug', slug] as const,
-<<<<<<< HEAD
-  level: () => [...keys.root(), 'level'] as const,
-=======
   position: () => [keys.root(), 'positions'] as const,
->>>>>>> 2a74b0f53c03da37660cff222ab77700eeb6bf23
 }
 
 export const useGetStaffs = () => {
@@ -41,18 +33,9 @@ export const useGetStaffDetail = (slug: string) => {
   })
 }
 
-<<<<<<< HEAD
-export const useGetStaffLevels = () => {
-  return useQuery({
-    queryKey: keys.level(),
-    queryFn: () => getStaffLevels()
-  })
-}
-=======
 export const useGetStaffPositions = () => {
   return useQuery({
     queryKey: keys.position(),
     queryFn: getStaffPositions,
   })
 }
->>>>>>> 2a74b0f53c03da37660cff222ab77700eeb6bf23
