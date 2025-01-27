3726bb19b5020d395f873283f54f55fee809c63e
ma1abakiev <malabakievramzan@gmail.com>
2025-01-27 12:04:23 +0600

    change title docko

--- src/pages/department/deparment-page.ui.tsx
+++ src/pages/department/deparment-page.ui.tsx
@@ -43,7 +43,7 @@ const documents = [
     ],
   },
   {
-    title: 'Планы ДОККО',
+    title: 'Документы ДОККО',
     urls: [
       {
         url: 'https://muit.makalabox.com/documents/%D0%A4%D1%83%D0%BD%D0%BA%D1%86.%D0%9E%D0%B1%D1%8F%D0%B7%D0%B0%D0%BD.%D0%9D%D0%B0%D1%87%D0%B0%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA%20(2).pdf',
