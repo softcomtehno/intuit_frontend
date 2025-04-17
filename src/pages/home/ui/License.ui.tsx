d752101d6dca394338f44bb6b441f543094b0ca5
ma1abakiev <malabakievramzan@gmail.com>
2025-04-17 16:31:25 +0600

    check license

--- src/pages/home/ui/License.ui.tsx
+++ src/pages/home/ui/License.ui.tsx
@@ -1,7 +1,7 @@
-import Fancybox from './Fancybox';
-import Gerb from '../../../assets/gerb.svg';
-import { Typography } from '@mui/material';
-import { t } from 'i18next';
+import Fancybox from './Fancybox'
+import Gerb from '../../../assets/gerb.svg'
+import { Typography } from '@mui/material'
+import { t } from 'i18next'
 
 export const License = () => {
   return (
@@ -11,7 +11,7 @@ export const License = () => {
         component="h3"
         className="text-[2.5rem] mb-5 font-semibold text-[#333] lg:text-[40px] md:!text-[30px]"
       >
-        {t("homepage.License.documents")}
+        {t('homepage.License.documents')}
       </Typography>
       <Fancybox
         options={{
@@ -24,16 +24,16 @@ export const License = () => {
           <div className="max-w-md w-full border border-gray rounded-lg bg-white p-2 shadow hover:shadow-lg transition-shadow duration-300 min-h-[150px]">
             <div className="flex flex-col items-center text-center">
               <h3 className="text-xl font-bold text-gray-800">
-                {t("homepage.License.scholarship")}
+                {t('homepage.License.scholarship')}
               </h3>
               <p className="text-sm text-gray-600 mt-4">
-                {t("homepage.License.content")}
+                {t('homepage.License.content')}
               </p>
             </div>
           </div>
           <a
             data-fancybox="gallery"
-            href="https://muit.makalabox.com/documents/License.pdf"
+            href="https://muit.makalabox.com/documents/license.pdf"
             className="flex items-center border border-gray rounded-lg bg-white p-6 shadow hover:shadow-lg transition-shadow duration-300 min-h-[150px]"
           >
             <div className="flex  items-center justify-center gap-3">
@@ -41,10 +41,12 @@ export const License = () => {
                 <img src={Gerb} alt="" />
               </div>
               <div>
-              <h3 className="text-lg font-semibold text-gray-800">{t("homepage.License.licenses")}</h3>
-              <p className="text-sm text-gray-600 mt-2">
-                {t("homepage.License.viewDocument")}
-              </p>
+                <h3 className="text-lg font-semibold text-gray-800">
+                  {t('homepage.License.licenses')}
+                </h3>
+                <p className="text-sm text-gray-600 mt-2">
+                  {t('homepage.License.viewDocument')}
+                </p>
               </div>
             </div>
           </a>
@@ -58,17 +60,17 @@ export const License = () => {
                 <img src={Gerb} alt="" />
               </div>
               <div>
-              <h3 className="text-lg font-semibold text-gray-800">
-                {t("homepage.License.certificate")}
-              </h3>
-              <p className="text-sm text-gray-600 mt-2">
-              {t("homepage.License.viewDocument")}
-              </p>
+                <h3 className="text-lg font-semibold text-gray-800">
+                  {t('homepage.License.certificate')}
+                </h3>
+                <p className="text-sm text-gray-600 mt-2">
+                  {t('homepage.License.viewDocument')}
+                </p>
               </div>
             </div>
           </a>
         </div>
       </Fancybox>
     </div>
-  );
-};
+  )
+}
