fee9ec933a20533624496ac87f516268f49ea009
ma1abakiev <malabakievramzan@gmail.com>
2025-05-18 16:45:35 +0600

    add: banner for event

--- src/widgets/events-list/event-list.ui.tsx
+++ src/widgets/events-list/event-list.ui.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material'
 import Marquee from 'react-fast-marquee'
-import { PartnerCard } from '~entities/partner'
 import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
 import { EventCard, eventQueries, eventTypes } from '~entities/events'
 import { Loader } from '~shared/ui/loader'
 
@@ -23,19 +23,41 @@ export const EventList = () => {
   }
   if (isSuccess) {
     return (
-      <Box className="w-full overflow-hidden my-10">
-        <Marquee
-          direction="right"
-          speed={100}
-          pauseOnHover={true}
-          className="mb-3 "
+      <>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: eventsData.data[0].title,
+          }}
+          className="text-center text-2xl font-bold text-[#333] "
+        ></div>
+        <section
+          className="w-full h-[600px] rounded-md bg-cover bg-center bg-no-repeat mt-5 flex justify-end items-end p-10"
+          style={{ backgroundImage: `url(${eventsData.data[0].banner})` }}
         >
-          {eventsData &&
-            eventsData.data.map((event: eventTypes.Event) => (
-              <EventCard key={event.slug} {...event}></EventCard>
-            ))}
-        </Marquee>
-      </Box>
+          <Link
+            className="rounded text-white inline-block bg-green p-2 hover:bg-green-600 transition"
+            to={`/news/event/${eventsData.data[0].slug}`}
+          >
+            Узнать больше
+          </Link>
+        </section>
+        <div className="uppercase text-2xl font-bold text-[#333] mt-10">
+          Другие Мероприятия
+        </div>
+        <Box className="w-full overflow-hidden my-10">
+          <Marquee
+            direction="right"
+            speed={100}
+            pauseOnHover={true}
+            className="mb-3 "
+          >
+            {eventsData &&
+              eventsData.data.map((event: eventTypes.Event) => (
+                <EventCard key={event.slug} {...event}></EventCard>
+              ))}
+          </Marquee>
+        </Box>
+      </>
     )
   }
 }
